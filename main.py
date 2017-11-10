1a121a39e6ed6bf7b04c555669647105fce7bdb8
DESKTOP-M6QDGNG\haxshaan <shantam.m22@gmail.com>
2017-11-10 08:40:46 +0530

    Rolling table feature added Fixed cookie bug Lots of Major bug fixes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os, sys, pickle, time
-
-from random import uniform
+import os
+import pickle
+import time
 
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
@@ -11,6 +11,7 @@ from selenium.webdriver.common.by import By
 
 binary = FirefoxBinary(r"C:\Program Files\Mozilla Firefox\firefox.exe")
 captcha_page = "https://freebitco.in/"
+dump_location = "dumps/cookies/"
 
 
 class HaxBitCoins(object):
@@ -25,9 +26,9 @@ class HaxBitCoins(object):
             return False
         return True
 
-    def wait_for_element(self, xpath, time):
+    def wait_for_element(self, xpath, t):
         try:
-            WebDriverWait(self.driver, time).until(EC.presence_of_element_located((By.XPATH, xpath)))
+            WebDriverWait(self.driver, t).until(EC.presence_of_element_located((By.XPATH, xpath)))
         except NoSuchElementException or TimeoutException:
             return False
         return True
@@ -47,22 +48,20 @@ class HaxBitCoins(object):
         try:
             self.driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div/div/span/div[5]')
             print("Captcha found!")
-        except:
+        except NoSuchElementException:
             print("No Captcha found")
             return False
+
         return True
 
     def wait_for_captcha(self):
 
         input("Press any Key to continue....")
 
-
     def load_url(self, url):
         self.driver.get(url)
 
     def login_homepage(self):
-        # Get url
-        self.load_url(captcha_page)
 
         # Wait for Login Script to appear then click
         if self.wait_for_element('/html/body/div[2]/div/nav/section/ul/li[10]/a', 10):
@@ -79,13 +78,39 @@ class HaxBitCoins(object):
             time.sleep(2)
 
     def roll_table(self):
+
+        # Go to roll tab
+
+        if self.wait_for_element('/html/body/div[2]/div/nav/section/ul/li[2]/a', 3):
+            self.driver.find_element_by_xpath('/html/body/div[2]/div/nav/section/ul/li[2]/a').click()
+        else:
+            print("Can't see earnBTC tab")
+
         # Wait for roll table
+
         if self.wait_for_element('//*[@id="free_play_payout_table"]', 10):
-            self.wait_for_captcha()
-            try:
-                self.wait_for_element('//*[@id="free_play_form_button"]', 10)
-                self.driver.find_element_by_xpath('roll buton').click()
-                print("Succesfully Rolled.")
+
+            # Check if Roll available:
+            if self.wait_for_element('//*[@id="wait"]', 4):
+                print("Roll countdown detected!")
+
+            else:
+
+                self.wait_for_captcha()
+                try:
+                    self.wait_for_element('//*[@id="free_play_form_button"]', 10)
+                    self.driver.find_element_by_xpath('//*[@id="free_play_form_button"]').click()
+                    print("Succesfully Rolled.")
+
+                except NoSuchElementException or TimeoutException:
+                    print("Roll Table Button not visible on page!")
+
+                # See if modal popup appeared
+                if self.wait_for_element('//*[@id="myModal22"]', 4):
+                    self.driver.find_element_by_xpath('/html/body/div[11]/a').click()
+                else:
+                    pass
+
                 if self.wait_for_element('//*[@id="free_play_digits"]', 5):
                     i = self.driver.find_element_by_xpath('//*[@id="free_play_first_digit"]').text
                     ii = self.driver.find_element_by_xpath('//*[@id="free_play_second_digit"]').text
@@ -93,36 +118,118 @@ class HaxBitCoins(object):
                     iv = self.driver.find_element_by_xpath('//*[@id="free_play_fourth_digit"]').text
                     v = self.driver.find_element_by_xpath('//*[@id="free_play_fifth_digit"]').text
 
-                    print("\nYou Got: %s %s %s %s %s") %(i, ii, iii, iv, v)
+                    print("\nYou Got: %r %r %r %r %r") % (i, ii, iii, iv, v)
                 else:
                     print("Can't find reward!")
-            except:
-                print("Roll Table Button not visible on page!")
+        else:
+            print("Can't Find Roll table!!")
 
+        self.driver.quit()
 
-
-    def save_session(self):
-
+    def save_session(self, cookie_name):
         cookies = self.driver.get_cookies()
-        pickle.dump(cookies, open("shaanhax.pkl", "wb"))
+        pickle.dump(cookies, open(cookie_name, "wb"))
 
-    def load_session(self):
-
-        cookies = pickle.load(open("shaanhax.pkl", "rb"))
+    def load_session(self, cookie_name):
+        cookies = pickle.load(open(cookie_name, "rb"))
 
         for cookie in cookies:
             self.driver.add_cookie(cookie)
 
 
+def check_pikle(new_ac_list):
+    """
+    :param new_ac_list: New dict object
+    :return: A list containing new keys
+    """
+    account_time_dict = pickle.load(open("dumps/accounts_map.hk", "rb"))
+
+    new_keys = []
+    for x in new_ac_list:
+        if x not in account_time_dict.keys():
+            new_keys.append(x)
+    return new_keys
+
+
 def main():
+
+    counter = 0
+
+    if os.path.getsize("accounts.txt") == 0:
+        print("Account file is empty!")
+        raise SystemExit(0)
+
+    try:
+        acc_file = open("accounts.txt", "r")
+    except:
+        print("Accounts.txt file not found. Refer documentation!")
+        raise SystemExit(0)
+
+    ac_list = []
+
+    for line in acc_file.readlines():
+        acname = line.split('@')[0]
+        ac_list.append(acname)
+
+    if not os.path.isfile("dumps/accounts_map.hk"):
+        print("No previous account dump detected creating fresh..")
+        acc_time = {}
+
+        for ac in ac_list:
+            acc_time[ac] = '%f' % 0.00
+
+        pickle.dump(acc_time, open("dumps/accounts_map.hk", 'wb'))
+        accounts_time = acc_time
+
+    else:
+        print("Previous accounts dump found.")
+        print("\nGetting new accounts from txt file...")
+        old_dict = pickle.load(open("dumps/accounts_map.hk", "rb"))
+        new_accounts = check_pikle(ac_list)
+        if len(new_accounts):
+            for item in new_accounts:
+                old_dict.update({item: 0.00})
+
+            pickle.dump(old_dict, open("dumps/accounts_map.hk", "wb"))
+        else:
+            print("No new account found.")
+        accounts_time = old_dict
 
     HaxObject = HaxBitCoins()
 
-    HaxObject.load_url(captcha_page)
-    time.sleep(5)
-    HaxObject.load_session()
-    HaxObject.load_url(captcha_page)
-    HaxObject.roll_table()
+    while True:
+        t1 = time.time()
+
+        for acc in accounts_time.keys():
+            t2 = float(accounts_time[acc])
+            time_left = t1 - t2
+            if time_left > 60*60 or time_left == t1:
+
+                time.sleep(4)
+
+                HaxObject.load_url(captcha_page)
+
+                if os.path.isfile(dump_location + acc + ".hax"):
+                    HaxObject.load_session(dump_location + acc + ".hax")
+                    HaxObject.load_url(captcha_page)
+                    time.sleep(4)
+                    HaxObject.roll_table()
+                    HaxObject.save_session(dump_location + acc + ".hax")
+                else:
+                    HaxObject.login_homepage()
+                    time.sleep(4)
+                    HaxObject.save_session(dump_location + acc + ".hax")
+                    HaxObject.roll_table()
+                    HaxObject.save_session(dump_location + acc + ".hax")
+
+                counter += 1
+                continue
+
+            else:
+                print("Waiting for Next challenge!")
+                time.sleep(10)
+                print("Time left: %s") % (str(time_left))
+                continue
 
 
 if __name__ == '__main__':
